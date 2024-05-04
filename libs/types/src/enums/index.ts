18eb9e1fe53aae71544713a42b56d0cbb2d0489d
Quan0308 <bddquan@gmail.com>
2024-05-04 20:29:42 +0700

    GUB-12 fix: rename user enum import

--- libs/types/src/enums/index.ts
+++ libs/types/src/enums/index.ts
@@ -1,4 +1,4 @@
-export * from "./role.enum";
+export * from "./user-role.enum";
 export * from "./business-error-code.enum";
 export * from "./booking-status.enum";
 export * from "./firebase-error-code.enum";
