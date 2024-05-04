a15e880f157808f0a9e97db62112e99028d3a2d4
Quan0308 <bddquan@gmail.com>
2024-05-04 11:29:33 +0700

    GUB-14 feat: create transactions table

--- libs/types/src/enums/index.ts
+++ libs/types/src/enums/index.ts
@@ -1,4 +1,5 @@
 export * from "./role.enum";
 export * from "./business-error-code.enum";
 export * from "./booking-status.enum";
-export * from "./firebase-error-code.enum"
+export * from "./firebase-error-code.enum";
+export * from "./transaction-status.enum";
